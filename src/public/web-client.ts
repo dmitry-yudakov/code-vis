7665d5f610d44bdf41beee9751a2c57e18acdfef
Dmitry Yudakov <dmitry.yudakov@opencode.com>
2019-05-27 19:37:40 +0300

    web-client.ts formatted by prettier

--- src/public/web-client.ts
+++ src/public/web-client.ts
@@ -4,32 +4,54 @@ declare var webkitSpeechRecognitionEvent;
 declare var window;
 declare var WebSocket;
 
-var SpeechRecognition = SpeechRecognition || (typeof webkitSpeechRecognition !== 'undefined' ? webkitSpeechRecognition : undefined)
-var SpeechGrammarList = SpeechGrammarList || (typeof webkitSpeechGrammarList !== 'undefined' ? webkitSpeechGrammarList : undefined)
-var SpeechRecognitionEvent = SpeechRecognitionEvent || (typeof webkitSpeechRecognitionEvent !== 'undefined' ? webkitSpeechRecognitionEvent : undefined)
+var SpeechRecognition =
+    SpeechRecognition ||
+    (typeof webkitSpeechRecognition !== 'undefined'
+        ? webkitSpeechRecognition
+        : undefined);
+var SpeechGrammarList =
+    SpeechGrammarList ||
+    (typeof webkitSpeechGrammarList !== 'undefined'
+        ? webkitSpeechGrammarList
+        : undefined);
+var SpeechRecognitionEvent =
+    SpeechRecognitionEvent ||
+    (typeof webkitSpeechRecognitionEvent !== 'undefined'
+        ? webkitSpeechRecognitionEvent
+        : undefined);
 
 var grammar;
 const commands = [
-    'open', 'up', 'down', 'go back', 'next', 'back',
-    'отвори', 'нагоре',
-]
+    'open',
+    'up',
+    'down',
+    'go back',
+    'next',
+    'back',
+    'отвори',
+    'нагоре'
+];
 const reinitGrammar = (keywords = []) => {
-    grammar = '#JSGF V1.0; grammar items; public <item> = ' + commands.concat(keywords).join(' | ') + ' ;'
-}
-reinitGrammar()
+    grammar =
+        '#JSGF V1.0; grammar items; public <item> = ' +
+        commands.concat(keywords).join(' | ') +
+        ' ;';
+};
+reinitGrammar();
 
 class Recognizer {
-    recognition: any
+    recognition: any;
 
-    constructor(protected conf: { onResult, continuous?, interimResults?, lang?, }) {
-    }
+    constructor(
+        protected conf: { onResult; continuous?; interimResults?; lang? }
+    ) {}
 
     start(grammar) {
-        this.stop()// just in case
+        this.stop(); // just in case
 
         let recognition = new SpeechRecognition();
         var speechRecognitionList = new SpeechGrammarList();
-        speechRecognitionList.addFromString(grammar, 1)
+        speechRecognitionList.addFromString(grammar, 1);
         console.log('grammar:', grammar);
         recognition.grammars = speechRecognitionList;
         recognition.continuous = this.conf.continuous || false;
@@ -37,24 +59,24 @@ class Recognizer {
         recognition.lang = this.conf.lang || 'en-US';
         // recognition.lang = 'bg-BG';
         // recognition.maxAlternatives = 1;
-        recognition.onresult = this.onresult.bind(this)
+        recognition.onresult = this.onresult.bind(this);
         // recognition.onspeechend = this.onspeechend.bind(this)
-        recognition.onend = this.onspeechend.bind(this)
-        recognition.onnomatch = this.onnomatch.bind(this)
-        recognition.onerror = this.onerror.bind(this)
-        recognition.start()
+        recognition.onend = this.onspeechend.bind(this);
+        recognition.onnomatch = this.onnomatch.bind(this);
+        recognition.onerror = this.onerror.bind(this);
+        recognition.start();
 
-        this.recognition = recognition
+        this.recognition = recognition;
     }
 
     stop() {
         if (this.recognition) {
-            this.recognition.stop()
+            this.recognition.stop();
         }
     }
 
     static isSupported() {
-        return (SpeechGrammarList != undefined)
+        return SpeechGrammarList != undefined;
     }
 
     private onresult(event) {
@@ -66,13 +88,13 @@ class Recognizer {
         // These also have getters so they can be accessed like arrays.
         // The [0] returns the SpeechRecognitionAlternative at position 0.
         // We then return the transcript property of the SpeechRecognitionAlternative object
-        console.log('results', event.results)
+        console.log('results', event.results);
         var last = event.results.length - 1;
         var res = event.results[last][0].transcript;
 
-        console.log('Result received: ' + res)
+        console.log('Result received: ' + res);
         console.log('Confidence: ' + event.results[0][0].confidence);
-        this.conf.onResult(null, res)
+        this.conf.onResult(null, res);
 
         // console.log('On result - restart')
         // this.recognition.start();
@@ -80,31 +102,31 @@ class Recognizer {
 
     private onspeechend() {
         if (this.conf.continuous) {
-            console.log('On speech end - restart')
+            console.log('On speech end - restart');
             return this.recognition.start();
         }
-        this.recognition = null
+        this.recognition = null;
     }
 
     private onnomatch(event) {
-        this.conf.onResult("Sorry, I could not understand")
-        console.log('No match')
+        this.conf.onResult('Sorry, I could not understand');
+        console.log('No match');
     }
 
     private onerror(event) {
-        this.conf.onResult(event.error)
-        console.log('Error occurred in recognition: ' + event.error)
+        this.conf.onResult(event.error);
+        console.log('Error occurred in recognition: ' + event.error);
     }
 }
 
 const onResult = (err, res) => {
-    console.log('Recognized', err, res)
-    console.log('Send to websocket')
+    console.log('Recognized', err, res);
+    console.log('Send to websocket');
     if (err) {
-        return console.log('Error:', err)
+        return console.log('Error:', err);
     }
-    ws.send(JSON.stringify({ command: res }))
-}
+    ws.send(JSON.stringify({ command: res }));
+};
 
 // let rec = new Recognizer({
 //     continuous: true,
@@ -125,13 +147,14 @@ window.document.getElementById('command-form').onsubmit = e => {
 
 let ws;
 const reconnectws = () => {
-    let protocol = (window.location.protocol === 'http:' ? 'ws' : 'wss')
-    console.log('connect websocket')
-    ws = new WebSocket(`${protocol}://${window.location.host}`)
-    ws.onclose = () => setTimeout(() => {
-        console.log('reconnect websocket')
-        reconnectws()
-    }, 1000)
+    let protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    console.log('connect websocket');
+    ws = new WebSocket(`${protocol}://${window.location.host}`);
+    ws.onclose = () =>
+        setTimeout(() => {
+            console.log('reconnect websocket');
+            reconnectws();
+        }, 1000);
     ws.onmessage = e => {
         console.log('ws message received', e.data);
         let msg = JSON.parse(e.data);
