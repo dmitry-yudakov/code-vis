c5bc0e62be5a6eab11b04572343a4fbd8f231e47
Dmitry Yudakov <dmitry.yudakov@opencode.com>
2019-06-09 16:14:48 +0300

    Disabled language server, ot complicates stuff for the moment with no real benefit

--- src/extension.ts
+++ src/extension.ts
@@ -163,50 +163,50 @@ const openChrome = () => {
     // }
 };
 
-const startLanguageServer = context => {
-    let serverModule = context.asAbsolutePath(path.join('out', 'server.js'));
-    // The debug options for the server
-    let debugOptions = { execArgv: ['--nolazy', '--inspect=6019'] };
+// const startLanguageServer = context => {
+//     let serverModule = context.asAbsolutePath(path.join('out', 'server.js'));
+//     // The debug options for the server
+//     let debugOptions = { execArgv: ['--nolazy', '--inspect=6019'] };
 
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    let serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc },
-        debug: {
-            module: serverModule,
-            transport: TransportKind.ipc,
-            options: debugOptions
-        }
-    };
+//     // If the extension is launched in debug mode then the debug server options are used
+//     // Otherwise the run options are used
+//     let serverOptions: ServerOptions = {
+//         run: { module: serverModule, transport: TransportKind.ipc },
+//         debug: {
+//             module: serverModule,
+//             transport: TransportKind.ipc,
+//             options: debugOptions
+//         }
+//     };
 
-    // Options to control the language client
-    let clientOptions: LanguageClientOptions = {
-        // Register the server for plain text documents
-        documentSelector: [
-            { language: 'typescript' },
-            { language: 'typescriptreact' },
-            { language: 'javascript' },
-            { language: 'javascriptreact' }
-        ], // [{ scheme: 'file', language: 'plaintext' }],
-        synchronize: {
-            // Synchronize the setting section 'lspSample' to the server
-            configurationSection: 'code-ai',
-            // Notify the server about file changes to '.clientrc files contain in the workspace
-            // fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-            // fileEvents: vscode.workspace.createFileSystemWatcher(includeMask)
-            fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
-        }
-    };
+//     // Options to control the language client
+//     let clientOptions: LanguageClientOptions = {
+//         // Register the server for plain text documents
+//         documentSelector: [
+//             { language: 'typescript' },
+//             { language: 'typescriptreact' },
+//             { language: 'javascript' },
+//             { language: 'javascriptreact' }
+//         ], // [{ scheme: 'file', language: 'plaintext' }],
+//         synchronize: {
+//             // Synchronize the setting section 'lspSample' to the server
+//             configurationSection: 'code-ai',
+//             // Notify the server about file changes to '.clientrc files contain in the workspace
+//             // fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+//             // fileEvents: vscode.workspace.createFileSystemWatcher(includeMask)
+//             fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+//         }
+//     };
 
-    // Create the language client and start the client.
-    let client = new LanguageClient(
-        'code-ai',
-        'Code AI',
-        serverOptions,
-        clientOptions
-    );
-    return client.start();
-};
+//     // Create the language client and start the client.
+//     let client = new LanguageClient(
+//         'code-ai',
+//         'Code AI',
+//         serverOptions,
+//         clientOptions
+//     );
+//     return client.start();
+// };
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -241,11 +241,8 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable);
 
-    disposable = startLanguageServer(context);
-
-    // Push the disposable to the context's subscriptions so that the
-    // client can be deactivated on extension deactivation
-    context.subscriptions.push(disposable);
+    // disposable = startLanguageServer(context);
+    // context.subscriptions.push(disposable);
 
     // disposable = commands.registerCommand('extension.sayHello', () => {
     //     console.log('hello');
--- src/server.ts
+++ src/server.ts
@@ -1,185 +1,185 @@
-'use strict';
+// 'use strict';
 
-import {
-    IPCMessageReader,
-    IPCMessageWriter,
-    createConnection,
-    IConnection,
-    TextDocuments,
-    TextDocument,
-    Diagnostic,
-    DiagnosticSeverity,
-    InitializeResult,
-    TextDocumentPositionParams,
-    CompletionItem,
-    CompletionItemKind
-} from 'vscode-languageserver';
+// import {
+//     IPCMessageReader,
+//     IPCMessageWriter,
+//     createConnection,
+//     IConnection,
+//     TextDocuments,
+//     TextDocument,
+//     Diagnostic,
+//     DiagnosticSeverity,
+//     InitializeResult,
+//     TextDocumentPositionParams,
+//     CompletionItem,
+//     CompletionItemKind
+// } from 'vscode-languageserver';
 
-console.log('Server root!!');
+// console.log('Server root!!');
 
-// Create a connection for the server. The connection uses Node's IPC as a transport
-let connection: IConnection = createConnection(
-    new IPCMessageReader(process),
-    new IPCMessageWriter(process)
-);
+// // Create a connection for the server. The connection uses Node's IPC as a transport
+// let connection: IConnection = createConnection(
+//     new IPCMessageReader(process),
+//     new IPCMessageWriter(process)
+// );
 
-// Create a simple text document manager. The text document manager
-// supports full document sync only
-let documents: TextDocuments = new TextDocuments();
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-documents.listen(connection);
+// // Create a simple text document manager. The text document manager
+// // supports full document sync only
+// let documents: TextDocuments = new TextDocuments();
+// // Make the text document manager listen on the connection
+// // for open, change and close text document events
+// documents.listen(connection);
 
-// After the server has started the client sends an initilize request. The server receives
-// in the passed params the rootPath of the workspace plus the client capabilites.
-let workspaceRoot: string;
-connection.onInitialize(
-    (params): InitializeResult => {
-        console.log('!! Server connection.onInitialize', params);
-        workspaceRoot = params.rootPath;
-        params.workspaceFolders;
+// // After the server has started the client sends an initilize request. The server receives
+// // in the passed params the rootPath of the workspace plus the client capabilites.
+// let workspaceRoot: string;
+// connection.onInitialize(
+//     (params): InitializeResult => {
+//         console.log('!! Server connection.onInitialize', params);
+//         workspaceRoot = params.rootPath;
+//         params.workspaceFolders;
 
-        console.log('Documents', documents.all());
-        return {
-            capabilities: {
-                // Tell the client that the server works in FULL text document sync mode
-                textDocumentSync: documents.syncKind,
-                // Tell the client that the server support code complete
-                completionProvider: {
-                    resolveProvider: true
-                }
-            }
-        };
-    }
-);
+//         console.log('Documents', documents.all());
+//         return {
+//             capabilities: {
+//                 // Tell the client that the server works in FULL text document sync mode
+//                 textDocumentSync: documents.syncKind,
+//                 // Tell the client that the server support code complete
+//                 completionProvider: {
+//                     resolveProvider: true
+//                 }
+//             }
+//         };
+//     }
+// );
 
-// The content of a text document has changed. This event is emitted
-// when the text document first opened or when its content has changed.
-documents.onDidChangeContent(change => {
-    console.log('Server documents.onDidChangeContent', change);
-    validateTextDocument(change.document);
-});
+// // The content of a text document has changed. This event is emitted
+// // when the text document first opened or when its content has changed.
+// documents.onDidChangeContent(change => {
+//     console.log('Server documents.onDidChangeContent', change);
+//     validateTextDocument(change.document);
+// });
 
-// The settings interface describe the server relevant settings part
-interface Settings {
-    lspSample: ExampleSettings;
-}
+// // The settings interface describe the server relevant settings part
+// interface Settings {
+//     lspSample: ExampleSettings;
+// }
 
-// These are the example settings we defined in the client's package.json
-// file
-interface ExampleSettings {
-    maxNumberOfProblems: number;
-}
+// // These are the example settings we defined in the client's package.json
+// // file
+// interface ExampleSettings {
+//     maxNumberOfProblems: number;
+// }
 
-// hold the maxNumberOfProblems setting
-let maxNumberOfProblems: number;
-// The settings have changed. Is send on server activation
-// as well.
-connection.onDidChangeConfiguration(change => {
-    console.log('Server connection.onDidChangeConfiguration');
-    let settings = <Settings>change.settings;
-    // maxNumberOfProblems = settings.lspSample.maxNumberOfProblems || 100;
-    maxNumberOfProblems = 100;
-    // Revalidate any open text documents
-    documents.all().forEach(validateTextDocument);
-});
+// // hold the maxNumberOfProblems setting
+// let maxNumberOfProblems: number;
+// // The settings have changed. Is send on server activation
+// // as well.
+// connection.onDidChangeConfiguration(change => {
+//     console.log('Server connection.onDidChangeConfiguration');
+//     let settings = <Settings>change.settings;
+//     // maxNumberOfProblems = settings.lspSample.maxNumberOfProblems || 100;
+//     maxNumberOfProblems = 100;
+//     // Revalidate any open text documents
+//     documents.all().forEach(validateTextDocument);
+// });
 
-function validateTextDocument(textDocument: TextDocument): void {
-    console.log('validateTextDocument', textDocument.uri);
-    let diagnostics: Diagnostic[] = [];
-    let lines = textDocument.getText().split(/\r?\n/g);
-    let problems = 0;
-    for (var i = 0; i < lines.length && problems < maxNumberOfProblems; i++) {
-        let line = lines[i];
-        console.log('analize line', line);
-        let index = line.indexOf('gaga');
-        if (index >= 0) {
-            console.log('GAGA!!');
-            problems++;
-            diagnostics.push({
-                severity: DiagnosticSeverity.Warning,
-                range: {
-                    start: { line: i, character: index },
-                    end: { line: i, character: index + 10 }
-                },
-                message: `${line.substr(
-                    index,
-                    10
-                )} should be spelled TypeScript`,
-                source: 'ex'
-            });
-        }
-    }
-    // Send the computed diagnostics to VSCode.
-    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-}
+// function validateTextDocument(textDocument: TextDocument): void {
+//     console.log('validateTextDocument', textDocument.uri);
+//     let diagnostics: Diagnostic[] = [];
+//     let lines = textDocument.getText().split(/\r?\n/g);
+//     let problems = 0;
+//     for (var i = 0; i < lines.length && problems < maxNumberOfProblems; i++) {
+//         let line = lines[i];
+//         console.log('analize line', line);
+//         let index = line.indexOf('gaga');
+//         if (index >= 0) {
+//             console.log('GAGA!!');
+//             problems++;
+//             diagnostics.push({
+//                 severity: DiagnosticSeverity.Warning,
+//                 range: {
+//                     start: { line: i, character: index },
+//                     end: { line: i, character: index + 10 }
+//                 },
+//                 message: `${line.substr(
+//                     index,
+//                     10
+//                 )} should be spelled TypeScript`,
+//                 source: 'ex'
+//             });
+//         }
+//     }
+//     // Send the computed diagnostics to VSCode.
+//     connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+// }
 
-connection.onDidChangeWatchedFiles(_change => {
-    console.log('Server connection.onDidChangeWatchedFiles', _change);
-    // Monitored files have change in VSCode
-    connection.console.log('We recevied an file change event');
-});
+// connection.onDidChangeWatchedFiles(_change => {
+//     console.log('Server connection.onDidChangeWatchedFiles', _change);
+//     // Monitored files have change in VSCode
+//     connection.console.log('We recevied an file change event');
+// });
 
-// This handler provides the initial list of the completion items.
-connection.onCompletion(
-    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-        // The pass parameter contains the position of the text document in
-        // which code complete got requested. For the example we ignore this
-        // info and always provide the same completion items.
-        return [
-            {
-                label: 'TypeScript',
-                kind: CompletionItemKind.Text,
-                data: 1
-            },
-            {
-                label: 'JavaScript',
-                kind: CompletionItemKind.Text,
-                data: 2
-            }
-        ];
-    }
-);
+// // This handler provides the initial list of the completion items.
+// connection.onCompletion(
+//     (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+//         // The pass parameter contains the position of the text document in
+//         // which code complete got requested. For the example we ignore this
+//         // info and always provide the same completion items.
+//         return [
+//             {
+//                 label: 'TypeScript',
+//                 kind: CompletionItemKind.Text,
+//                 data: 1
+//             },
+//             {
+//                 label: 'JavaScript',
+//                 kind: CompletionItemKind.Text,
+//                 data: 2
+//             }
+//         ];
+//     }
+// );
 
-// This handler resolve additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve(
-    (item: CompletionItem): CompletionItem => {
-        if (item.data === 1) {
-            (item.detail = 'TypeScript details'),
-                (item.documentation = 'TypeScript documentation');
-        } else if (item.data === 2) {
-            (item.detail = 'JavaScript details'),
-                (item.documentation = 'JavaScript documentation');
-        }
-        return item;
-    }
-);
+// // This handler resolve additional information for the item selected in
+// // the completion list.
+// connection.onCompletionResolve(
+//     (item: CompletionItem): CompletionItem => {
+//         if (item.data === 1) {
+//             (item.detail = 'TypeScript details'),
+//                 (item.documentation = 'TypeScript documentation');
+//         } else if (item.data === 2) {
+//             (item.detail = 'JavaScript details'),
+//                 (item.documentation = 'JavaScript documentation');
+//         }
+//         return item;
+//     }
+// );
 
-connection.onDidOpenTextDocument(params => {
-    // A text document got opened in VSCode.
-    // params.uri uniquely identifies the document. For documents store on disk this is a file URI.
-    // params.text the initial full content of the document.
-    connection.console.log(`${params.textDocument.uri} opened.`);
-    console.log('Documents count', documents.all().length);
-});
-connection.onDidChangeTextDocument(params => {
-    // The content of a text document did change in VSCode.
-    // params.uri uniquely identifies the document.
-    // params.contentChanges describe the content changes to the document.
-    connection.console.log(
-        `${params.textDocument.uri} changed: ${JSON.stringify(
-            params.contentChanges
-        )}`
-    );
-    console.log('documents', documents.all());
-    // validateTextDocument(params.contentChanges)
-});
-connection.onDidCloseTextDocument(params => {
-    // A text document got closed in VSCode.
-    // params.uri uniquely identifies the document.
-    connection.console.log(`${params.textDocument.uri} closed.`);
-});
+// connection.onDidOpenTextDocument(params => {
+//     // A text document got opened in VSCode.
+//     // params.uri uniquely identifies the document. For documents store on disk this is a file URI.
+//     // params.text the initial full content of the document.
+//     connection.console.log(`${params.textDocument.uri} opened.`);
+//     console.log('Documents count', documents.all().length);
+// });
+// connection.onDidChangeTextDocument(params => {
+//     // The content of a text document did change in VSCode.
+//     // params.uri uniquely identifies the document.
+//     // params.contentChanges describe the content changes to the document.
+//     connection.console.log(
+//         `${params.textDocument.uri} changed: ${JSON.stringify(
+//             params.contentChanges
+//         )}`
+//     );
+//     console.log('documents', documents.all());
+//     // validateTextDocument(params.contentChanges)
+// });
+// connection.onDidCloseTextDocument(params => {
+//     // A text document got closed in VSCode.
+//     // params.uri uniquely identifies the document.
+//     connection.console.log(`${params.textDocument.uri} closed.`);
+// });
 
-// Listen on the connection
-connection.listen();
+// // Listen on the connection
+// connection.listen();
