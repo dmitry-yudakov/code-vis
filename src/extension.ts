0bc9013a2af19337c6cc608fa2b41a29612a989c
Dmitry Yudakov <dmitry.yudakov@opencode.com>
2019-07-11 13:46:41 +0300

    Command 'project map' changed to 'map project'
    Later it could be 'map smt-else'

--- src/extension.ts
+++ src/extension.ts
@@ -101,9 +101,13 @@ function onCommand(command) {
         case 'open':
             if (tokens.length) openFile(tokens);
             break;
-        case 'project':
-            contributeCommandsHandlers['codeai.projectMap']();
-            break;
+        case 'map': {
+            const what = tokens.shift();
+            if (what === 'project') {
+                contributeCommandsHandlers['codeai.projectMap']();
+                break;
+            }
+        }
         default:
             vscode.window.showWarningMessage(
                 'Could not recognize command: "' + command + '"'
