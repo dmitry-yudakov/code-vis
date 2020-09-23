1f83bc399dac12d3b0ab50b3a2e2284777d4fa48
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2020-09-23 20:45:45 +0300

    few warnings fixed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { WSConn } from './connection';
 import ReactFlow, { Background } from 'react-flow-renderer';
