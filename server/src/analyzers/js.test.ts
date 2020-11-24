fb85b38b01bc701af169827a9ab548525bd82dc7
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2020-11-24 17:24:18 +0200

    Added tests for server covering extracting includes

---
+++ server/src/analyzers/js.test.ts
@@ -0,0 +1,209 @@
+import jsAnalyzer from './js';
+
+describe('Includes using "import"', () => {
+  test('from same directory', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(['src/dir/a.js'], async (filename) => {
+        expect(filename).toBe('src/dir/a.js');
+        return `import gaga from './b.js'`;
+      })
+    ).resolves.toEqual([
+      { from: 'src/dir/b.js', to: 'src/dir/a.js', items: ['gaga'] },
+    ]));
+
+  test('from different subdirectory', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `import gaga from '../dir2/b.js'`
+      )
+    ).resolves.toEqual([
+      { from: 'src/dir2/b.js', to: 'src/dir/a.js', items: ['gaga'] },
+    ]));
+
+  test('from different directory', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `import gaga from '../../src2/dir2/b.js'`
+      )
+    ).resolves.toEqual([
+      { from: 'src2/dir2/b.js', to: 'src/dir/a.js', items: ['gaga'] },
+    ]));
+
+  test('non-default exports', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `import { gaga, maga } from './b.js'`
+      )
+    ).resolves.toEqual([
+      { from: 'src/dir/b.js', to: 'src/dir/a.js', items: ['gaga', 'maga'] },
+    ]));
+
+  test('mixed exports', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `import { gaga, maga }, yaga from './b.js'`
+      )
+    ).resolves.toEqual([
+      {
+        from: 'src/dir/b.js',
+        to: 'src/dir/a.js',
+        items: ['gaga', 'maga', 'yaga'],
+      },
+    ]));
+
+  test('multiple imports', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `
+      import { gaga, maga } from './b.js';
+      import djaga from './c.js';
+      `
+      )
+    ).resolves.toEqual([
+      {
+        from: 'src/dir/b.js',
+        to: 'src/dir/a.js',
+        items: ['gaga', 'maga'],
+      },
+      {
+        from: 'src/dir/c.js',
+        to: 'src/dir/a.js',
+        items: ['djaga'],
+      },
+    ]));
+
+  // test('multiple lines', () =>
+  //   expect(
+  //     jsAnalyzer.extractFilesHierarchy(
+  //       ['src/dir/a.js'],
+  //       async () => `
+  //     import {
+  //       gaga,
+  //       maga
+  //     } from './b.js';
+  //     `
+  //     )
+  //   ).resolves.toEqual([
+  //     {
+  //       from: 'src/dir/b.js',
+  //       to: 'src/dir/a.js',
+  //       items: ['gaga', 'maga'],
+  //     },
+  //   ]));
+});
+
+describe('Includes using "require"', () => {
+  test('from same directory', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(['src/dir/a.js'], async (filename) => {
+        expect(filename).toBe('src/dir/a.js');
+        return `const gaga = require('./b.js')`;
+      })
+    ).resolves.toEqual([
+      { from: 'src/dir/b.js', to: 'src/dir/a.js', items: ['gaga'] },
+    ]));
+
+  test('from different subdirectory', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `const gaga = require('../dir2/b.js')`
+      )
+    ).resolves.toEqual([
+      { from: 'src/dir2/b.js', to: 'src/dir/a.js', items: ['gaga'] },
+    ]));
+
+  test('from different directory', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `const gaga = require('../../src2/dir2/b.js')`
+      )
+    ).resolves.toEqual([
+      { from: 'src2/dir2/b.js', to: 'src/dir/a.js', items: ['gaga'] },
+    ]));
+
+  test('non-default exports', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `const { gaga, maga } = require('./b.js')`
+      )
+    ).resolves.toEqual([
+      { from: 'src/dir/b.js', to: 'src/dir/a.js', items: ['gaga', 'maga'] },
+    ]));
+
+  test('multiple requires', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `
+      const { gaga, maga } = require('./b.js');
+      const djaga = require('./c.js');
+      `
+      )
+    ).resolves.toEqual([
+      {
+        from: 'src/dir/b.js',
+        to: 'src/dir/a.js',
+        items: ['gaga', 'maga'],
+      },
+      {
+        from: 'src/dir/c.js',
+        to: 'src/dir/a.js',
+        items: ['djaga'],
+      },
+    ]));
+
+  test('var/let/const', () =>
+    expect(
+      jsAnalyzer.extractFilesHierarchy(
+        ['src/dir/a.js'],
+        async () => `
+        const gaga = require('./b.js');
+        let djaga = require('./c.js');
+        var yaga = require('./d.js');
+        `
+      )
+    ).resolves.toEqual([
+      {
+        from: 'src/dir/b.js',
+        to: 'src/dir/a.js',
+        items: ['gaga'],
+      },
+      {
+        from: 'src/dir/c.js',
+        to: 'src/dir/a.js',
+        items: ['djaga'],
+      },
+      {
+        from: 'src/dir/d.js',
+        to: 'src/dir/a.js',
+        items: ['yaga'],
+      },
+    ]));
+
+  // test('multiple lines', () =>
+  //   expect(
+  //     jsAnalyzer.extractFilesHierarchy(
+  //       ['src/dir/a.js'],
+  //       async () => `
+  //     const {
+  //       gaga,
+  //       maga
+  //     } = require('./b.js');
+  //     `
+  //     )
+  //   ).resolves.toEqual([
+  //     {
+  //       from: 'src/dir/b.js',
+  //       to: 'src/dir/a.js',
+  //       items: ['gaga', 'maga'],
+  //     },
+  //   ]));
+});
