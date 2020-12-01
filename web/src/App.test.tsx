832826c261c7023ff951eddc8a6a3b210101e1b1
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2020-12-01 16:20:31 +0200

    test(web): removed not working app test, added test for few utils functions

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+import { getFilenameParts, isEmptyContent } from './utils';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Utils', () => {
+  test('detect empty space', () => {
+    expect(isEmptyContent('  ')).toBe(true);
+
+    expect(isEmptyContent('\t')).toBe(true);
+
+    expect(
+      isEmptyContent(`
+    
+    `)
+    ).toBe(true);
+
+    expect(
+      isEmptyContent(`function gaga() {
+
+        }
+
+    `)
+    ).toBe(false);
+
+    expect(
+      isEmptyContent(`
+    
+        function gaga() {
+
+        }
+
+    `)
+    ).toBe(false);
+  });
+
+  test('parse filename parts', () => {
+    expect(getFilenameParts('/src/client/App.tsx')).toEqual({
+      path: '/src/client/',
+      name: 'App',
+      ext: 'tsx',
+    });
+    expect(getFilenameParts('index.tsx')).toEqual({
+      path: '',
+      name: 'index',
+      ext: 'tsx',
+    });
+    expect(getFilenameParts('/src/client/Gaga')).toEqual({
+      path: '',
+      name: '/src/client/Gaga',
+      ext: '',
+    });
+    expect(getFilenameParts('GagaDjaga')).toEqual({
+      path: '',
+      name: 'GagaDjaga',
+      ext: '',
+    });
+  });
 });
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
