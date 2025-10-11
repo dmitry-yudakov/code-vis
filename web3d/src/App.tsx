0d02d91b29cfa9dc20dfd314c8a297b826ffa847
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2025-10-11 11:28:34 +0300

    some demo three fiber elements added

--- web3d/src/App.tsx
+++ web3d/src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useRef, useState } from 'react';
+import { Mesh } from 'three';
+
+const Map3d = () => {
+  const [hovered, setHover] = useState(false);
+  const ref = useRef<Mesh>();
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += hovered ? delta : delta / 5;
+      // ref.current.rotation.y += delta;
+      // ref.current.rotation.z += delta;
+    }
+  });
+
+  return (
+    <mesh
+      ref={ref as any}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[10, 5, 0.1]} />
+      <meshStandardMaterial color="hotpink" />
+    </mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    <Canvas>
+      <ambientLight />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+
+      <pointLight position={[-10, -10, -10]} />
+      <Map3d />
+      <OrbitControls />
+    </Canvas>
+    // </div>
   );
 }
 
